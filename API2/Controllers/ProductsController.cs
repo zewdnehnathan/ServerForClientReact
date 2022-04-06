3b7e89f7e6b4a0976ae1127c2a9a9c0a8ea4219b
Nathan Daniel <NDaniel@excellerentsolutions.com>
2022-04-07 02:36:41 +0300

    1stinitial

---
+++ API2/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using API2.Data;
+using API2.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductsController(StoreContext context)
+        {
+            _storeContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() 
+        {
+            return await _storeContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id) 
+        {
+            return await _storeContext.Products.FindAsync(id);
+        }
+
+    }
+}
---
+++ API2/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using API2.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API2.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    
+    
+    }
+}
